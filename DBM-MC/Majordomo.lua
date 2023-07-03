d12616745be8f9fe624cea33b671d70535cfad45
Balleny <12682004+balleny@users.noreply.github.com>
2023-07-03 20:20:17 +0200

    MC

--- DBM-MC/Majordomo.lua
+++ DBM-MC/Majordomo.lua
@@ -10,13 +10,11 @@ mod:RegisterCombat("combat")
 --mod:RegisterKill("yell", L.Kill)
 
 mod:RegisterEventsInCombat(
-	"SPELL_CAST_SUCCESS 20619 21075 20534"
+	"SPELL_CAST_SUCCESS 20619 21075 20534 20618"
 )
 
---[[
-(ability.id = 20619 or ability.id = 21075 or ability.id = 20534) and type = "cast"
---]]
 local warnTeleport			= mod:NewTargetNoFilterAnnounce(20534)
+local warnTeleportRandom	= mod:NewTargetNoFilterAnnounce(20618)
 local warnDamageShield		= mod:NewSpellAnnounce(21075, 2)
 
 local specWarnMagicReflect	= mod:NewSpecialWarningReflect(20619, "CasterDps", nil, 2, 1, 2)
@@ -24,31 +22,44 @@ local specWarnDamageShield	= mod:NewSpecialWarningReflect(21075, false, nil, 2, 
 
 local timerMagicReflect		= mod:NewBuffActiveTimer(10, 20619, nil, nil, nil, 5, nil, DBM_COMMON_L.DAMAGE_ICON)
 local timerDamageShield		= mod:NewBuffActiveTimer(10, 21075, nil, nil, nil, 5, nil, DBM_COMMON_L.DAMAGE_ICON)
-local timerTeleportCD		= mod:NewCDTimer(25, 20534, nil, nil, nil, 5, nil, DBM_COMMON_L.TANK_ICON)--25-30
-local timerShieldCD			= mod:NewTimer(30.3, "timerShieldCD", nil, nil, nil, 6, nil, DBM_COMMON_L.DAMAGE_ICON)
+local timerTeleportCD		= mod:NewCDTimer(25+5, 20534, nil, nil, nil, 5, nil, DBM_COMMON_L.TANK_ICON)
+local timerTeleportRandomCD = mod:NewCDTimer(30, 20618, nil, nil, nil, 5, nil, DBM_COMMON_L.TANK_ICON)
+local timerShieldCD			= mod:NewTimer(30.3-0.3, "timerShieldCD", nil, nil, nil, 6, nil, DBM_COMMON_L.DAMAGE_ICON)
 
 function mod:OnCombatStart(delay)
-	timerTeleportCD:Start(19.4-delay)
-	timerShieldCD:Start(27.8-delay)--27-30
+	timerTeleportCD:Start(19.4-4.4-delay)
+	timerTeleportRandomCD:Start(25-delay)
+	timerShieldCD:Start(27.8+2.2-delay)--27-30
 end
 
-function mod:SPELL_CAST_SUCCESS(args)
-	if args.spellId == 20619 then
-		specWarnMagicReflect:Show(BOSS)--Always a threat to casters
-		specWarnMagicReflect:Play("stopattack")
-		timerMagicReflect:Start()
-		timerShieldCD:Start()
-	elseif args.spellId == 21075 then
-		if self.Options.SpecWarn21075reflect and not self:IsTrivial() then--Not a threat to high level melee
-			specWarnDamageShield:Show(BOSS)
-			specWarnDamageShield:Play("stopattack")
-		else
-			warnDamageShield:Show()
+do
+	local MagicReflect, MeleeReflect, Teleport, TeleportRandom = DBM:GetSpellInfo(20619), DBM:GetSpellInfo(21075), DBM:GetSpellInfo(20534), DBM:GetSpellInfo(20618)
+	function mod:SPELL_CAST_SUCCESS(args)
+		--local spellId = args.spellId
+		local spellName = args.spellName
+		--if spellId == 20619 then
+		if spellName == MagicReflect then
+			specWarnMagicReflect:Show(BOSS)--Always a threat to casters
+			specWarnMagicReflect:Play("stopattack")
+			timerMagicReflect:Start()
+			timerShieldCD:Start()
+		--elseif spellId == 21075 then
+		elseif spellName == MeleeReflect then
+			if self.Options.SpecWarn21075reflect and not self:IsTrivial(75) then
+				specWarnDamageShield:Show(BOSS)
+				specWarnDamageShield:Play("stopattack")
+			else
+				warnDamageShield:Show()
+			end
+			timerDamageShield:Start()
+			timerShieldCD:Start()
+		--elseif spellId == 20534 then
+		elseif spellName == Teleport then
+			warnTeleport:Show(args.destName)
+			timerTeleportCD:Start()
+		elseif spellName == TeleportRandom then
+			warnTeleportRandom:Show(args.destName)
+			timerTeleportRandomCD:Start()
 		end
-		timerDamageShield:Start()
-		timerShieldCD:Start()
-	elseif args.spellId == 20534 then
-		warnTeleport:Show(args.destName)
-		timerTeleportCD:Start()
 	end
 end
--- DBM-MC/Ragnaros.lua
+++ DBM-MC/Ragnaros.lua
@@ -10,54 +10,39 @@ mod:RegisterCombat("combat")
 
 mod:RegisterEvents(
 	"SPELL_CAST_START 19774",
-	"SPELL_CAST_SUCCESS 19773",
-	"CHAT_MSG_MONSTER_YELL"
-)
-mod:RegisterEventsInCombat(
-	"SPELL_CAST_START 20568",
 	"SPELL_CAST_SUCCESS 20566",
+	"CHAT_MSG_MONSTER_YELL",
+	"SPELL_INSTAKILL 19773"
+)
+
+mod:RegisterEventsInCombat(
 	"UNIT_DIED"
 )
 
---[[
-ability.id = 20566 and type = "cast" or target.id = 12143 and type = "death"
---]]
-local warnWrathRag			= mod:NewSpellAnnounce(20566, 3)
-local warnSubmerge			= mod:NewSpellAnnounce(21107, 2, "Interface\\AddOns\\DBM-Core\\textures\\CryptFiendBurrow.blp")
-local warnEmerge			= mod:NewSpellAnnounce(20568, 2, "Interface\\AddOns\\DBM-Core\\textures\\CryptFiendUnBurrow.blp")
-local warnSonsOfFlameLeft	= mod:NewAddsLeftAnnounce(19629, 2, 19774) -- spellId 21108 (Summon of Sons of Flame) returns nil, so use a similar spellId: 19629 (Summon Flames)
+local warnWrathRag		= mod:NewSpellAnnounce(20566, 3)
+local warnSubmerge		= mod:NewAnnounce("WarnSubmerge", 2, "Interface\\AddOns\\DBM-Core\\textures\\CryptFiendBurrow.blp")
+local warnEmerge		= mod:NewAnnounce("WarnEmerge", 2, "Interface\\AddOns\\DBM-Core\\textures\\CryptFiendUnBurrow.blp")
 
-local timerWrathRag			= mod:NewCDTimer(22.1, 20566, nil, nil, nil, 2, nil, DBM_COMMON_L.IMPORTANT_ICON, true, mod:IsMelee() and 1, 4) -- ~7s variance [22.1-29.0]. Added "keep" arg. (40N Lordaeron [2023-09-13]@[19:05:07]) - "Wrath of Ragnaros-20566-npc:11502-303 = pull:29.98, 28.56, 25.76, 22.10, 22.73, 23.72, 69.84, 27.74, 22.44, 29.00, 25.42, 21.28, 67.54, 24.18, 28.92"
-local timerSubmerge			= mod:NewNextTimer(180, 21107, nil, nil, nil, 6, "Interface\\AddOns\\DBM-Core\\textures\\CryptFiendBurrow.blp", nil, nil, 1, 5)
-local timerEmerge			= mod:NewNextTimer(90, 20568, nil, nil, nil, 6, "Interface\\AddOns\\DBM-Core\\textures\\CryptFiendUnBurrow.blp", nil, nil, 1, 5)
-local timerCombatStart		= mod:NewCombatTimer(78)
+local timerWrathRag		= mod:NewCDTimer(25, 20566, nil, nil, nil, 2, nil, DBM_COMMON_L.IMPORTANT_ICON, nil, mod:IsMelee() and 1, 4)--25-31.6
+local timerSubmerge		= mod:NewTimer(180, "TimerSubmerge", "Interface\\AddOns\\DBM-Core\\textures\\CryptFiendBurrow.blp", nil, nil, 6, nil, nil, 1, 5)
+local timerEmerge		= mod:NewTimer(90, "TimerEmerge", "Interface\\AddOns\\DBM-Core\\textures\\CryptFiendUnBurrow.blp", nil, nil, 6, nil, nil, 1, 5)
+local timerCombatStart	= mod:NewTimer(78+2, "timerCombatStart", "Interface\\Icons\\Ability_Warrior_OffensiveStance", nil, nil, 6, nil, nil) -- 72.5
 
-mod:AddRangeFrameOption("18", nil, "-Melee")
+mod:AddRangeFrameOption("20") -- Blizz 18, AzerothCore +2 for regular chars, or 4 for male tauren/draenei
 
 mod.vb.addLeft = 0
---mod.vb.ragnarosEmerged = true
---local addsGuidCheck = {}
+mod.vb.ragnarosEmerged = true
+local addsGuidCheck = {}
 local firstBossMod = DBM:GetModByName("MCTrash")
 
---[[
-local function emerged(self)
-	DBM:AddSpecialEventToTranscriptorLog("Emerged")
-	self.vb.ragnarosEmerged = true
-	timerEmerge:Stop()
-	warnEmerge:Show()
-	timerWrathRag:Start(26.7)--need to find out what it is first.
-	timerSubmerge:Start(90) -- 180s from last Submerge, so account for the 90s from emerge timer. Submerge Yells diff (40N Lordaeron [2023-09-13]@[19:05:07]) - 2209.92 > 2389.91 [179,99]
-end
-]]
-
 function mod:OnCombatStart(delay)
---	table.wipe(addsGuidCheck)
+	table.wipe(addsGuidCheck)
 	self.vb.addLeft = 0
---	self.vb.ragnarosEmerged = true
-	timerWrathRag:Start(30-delay)
-	timerSubmerge:Start(180-delay) -- (40N Lordaeron [2023-09-13]@[19:05:07]) - 180
+	self.vb.ragnarosEmerged = true
+	timerWrathRag:Start(26.7+3.3-delay)
+	timerSubmerge:Start(180-delay)
 	if self.Options.RangeFrame then
-		DBM.RangeCheck:Show(18)
+		DBM.RangeCheck:Show(18+2) -- Blizz 10, AzerothCore +2 for regular chars, or 4 for male tauren/draenei
 	end
 end
 
@@ -88,91 +73,79 @@ function mod:OnCombatEnd(wipe)
 	end
 end
 
-function mod:SPELL_CAST_START(args)
-	local spellId = args.spellId
-	if spellId == 19774 and self:AntiSpam(5, 4) then
-		--This is still going to use a sync event because someone might start this RP from REALLY REALLY far away
-		self:SendSync("SummonRag")
-	elseif spellId == 20568 then -- Ragnaros Emerge
-		DBM:AddSpecialEventToTranscriptorLog("Emerged")
---		self.vb.ragnarosEmerged = true
-		timerEmerge:Stop()
-		warnEmerge:Show()
-		timerWrathRag:Start(30) -- REVIEW! (40N Lordaeron [2023-09-13]@[19:05:07]) - 2222.61 > 2252.60 [29.99]
-		-- Don't start Submerge timer here, since Ragnaros will emerge after 90 seconds from Submerge/Summon Sons of Flames OR once all 8 are defeated (whichever happens first). The latter is variable and therefore not suitable for any timer
+local function emerged(self)
+	self.vb.ragnarosEmerged = true
+	timerEmerge:Stop()
+	warnEmerge:Show()
+	timerWrathRag:Start(26.7+3.3)
+	timerSubmerge:Start(180)
+end
+
+do
+	local summonRag = DBM:GetSpellInfo(19774)
+	function mod:SPELL_CAST_START(args)
+		--if args.spellId == 20566 then
+		if args.spellName == summonRag and self:AntiSpam(5, 4) then
+			--This is still going to use a sync event because someone might start this RP from REALLY REALLY far away
+			self:SendSync("SummonRag")
+		end
 	end
 end
 
-function mod:SPELL_CAST_SUCCESS(args)
-	local spellId = args.spellId
-	if spellId == 20566 then
-		warnWrathRag:Show()
-		timerWrathRag:Start()
-	elseif spellId == 19773 then
-		--This is still going to use a sync event because someone might start this RP from REALLY REALLY far away
-		self:SendSync("DomoDeath")
+do
+	local Wrath, domoDeath = DBM:GetSpellInfo(20566), DBM:GetSpellInfo(19773)
+	function mod:SPELL_CAST_SUCCESS(args)
+		--if args.spellId == 20566 then
+		if args.spellName == Wrath then
+			warnWrathRag:Show()
+			timerWrathRag:Start()
+		end
+	end
+	
+	function mod:SPELL_INSTAKILL(args)
+		if args.spellName == domoDeath then
+			--This is still going to use a sync event because someone might start this RP from REALLY REALLY far away
+			self:SendSync("DomoDeath")
+		end
 	end
 end
-
 
 function mod:UNIT_DIED(args)
 	local guid = args.destGUID
-	if self:GetCIDFromGUID(guid) == 12143 then--Son of Flame
-		--self:SendSync("AddDied", guid)--Send sync it died do to combat log range and size of room
-		--We're in range of event, no reason to wait for sync, especially in a raid that might not have many DBM users
---		if not addsGuidCheck[guid] then
---			addsGuidCheck[guid] = true
+	local cid = self:GetCIDFromGUID(guid)
+	if cid == 12143 then--Son of Flame
+		if not addsGuidCheck[guid] then
+			addsGuidCheck[guid] = true
 			self.vb.addLeft = self.vb.addLeft - 1
---			if not self.vb.ragnarosEmerged and self.vb.addLeft == 0 then--After all 8 die he emerges immediately
---				self:Unschedule(emerged)
---				emerged(self)
---			end
---		end
-		warnSonsOfFlameLeft:Show(self.vb.addLeft)
+			if not self.vb.ragnarosEmerged and self.vb.addLeft == 0 then--After all 8 die he emerges immediately
+				self:Unschedule(emerged)
+				emerged(self)
+			end
+		end
 	end
 end
-
 
 function mod:CHAT_MSG_MONSTER_YELL(msg)
-	if msg == L.Submerge or msg == L.Submerge2 then
+	if msg == L.Submerge then
 		self:SendSync("Submerge")
-	elseif msg == L.Pull and self:AntiSpam(5, 4) then
-		self:SendSync("SummonRag")
+--	elseif msg == L.Pull and self:AntiSpam(5, 4) then
+--		self:SendSync("SummonRag")
 	end
 end
 
-function mod:OnSync(msg--[[, guid]])
+function mod:OnSync(msg, guid)
 	if msg == "SummonRag" and self:AntiSpam(5, 2) then
 		timerCombatStart:Start()
 	elseif msg == "Submerge" and self:IsInCombat() then
-		DBM:AddSpecialEventToTranscriptorLog("Submerged")
---		self.vb.ragnarosEmerged = false
---		self:Unschedule(emerged)
+		self.vb.ragnarosEmerged = false
+		self:Unschedule(emerged)
 		timerWrathRag:Stop()
-		timerSubmerge:Stop()
 		warnSubmerge:Show()
 		timerEmerge:Start(90)
-		timerSubmerge:Start() -- Submerge Yells diff (40N Lordaeron [2023-09-13]@[19:05:07]) - 2209.92 > 2389.91 [179.99]
---		self:Schedule(90, emerged, self)
+		self:Schedule(90, emerged, self)
 		self.vb.addLeft = self.vb.addLeft + 8
-	--[[elseif msg == "AddDied" and self:IsInCombat() and guid and not addsGuidCheck[guid] then
-		--A unit died we didn't detect ourselves, so we correct our adds counter from sync
-		addsGuidCheck[guid] = true
-		self.vb.addLeft = self.vb.addLeft - 1
-		if not self.vb.ragnarosEmerged and self.vb.addLeft == 0 then--After all 8 die he emerges immediately
-			self:Unschedule(emerged)
-			emerged(self)
-		end--]]
 	elseif msg == "DomoDeath" and self:AntiSpam(5, 3) then
-		--The timer between yell/summon start and ragnaros being attackable is variable, but time between domo death and him being attackable is not.
-		--As such, we start lowest timer of that variation on the RP start, but adjust timer if it's less than 10 seconds at time domo dies
-		local remaining = timerCombatStart:GetRemaining()
-		if remaining then
-			if remaining < 10 then
-				timerCombatStart:AddTime(10 - remaining)
-			elseif remaining > 10 then
-				timerCombatStart:RemoveTime(remaining - 10)
-			end
-		end
+		timerCombatStart:Stop()
+		timerCombatStart:Start(7)
 	end
 end
